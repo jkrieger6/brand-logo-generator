31d318e21d4ec152826c1700b3671715618cd644
jkrieger6 <jkrieger11@gmail.com>
2023-04-20 17:27:52 -0700

    added path package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Packages to run application
 const inquierer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 // Package to run tests
 const jest = require('jest');
