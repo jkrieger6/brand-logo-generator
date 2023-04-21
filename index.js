2d10c498d632b73dce4cd275daa42b4302a13401
jkrieger6 <jkrieger11@gmail.com>
2023-04-20 17:33:39 -0700

    added array of questions for user input

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const fs = require('fs');
 const path = require('path');
 // Package to run tests
 const jest = require('jest');
+
+const generateCVG = require('./src/generateCVG');
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What are three characters that represent your vision? (Required)",
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: "Enter a color for the background of your logo (Required)",
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        choices: ['circle', 'square', 'triangle']
+    }
+];
