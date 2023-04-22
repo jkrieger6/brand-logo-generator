f7ac9ef0f8b7483fd2457dbfb87d91fae8339ad9
jkrieger6 <jkrieger11@gmail.com>
2023-04-22 09:28:39 -0700

    added if else statements to decide user input choices

--- assets/shapes.js
+++ assets/shapes.js
@@ -3,14 +3,68 @@ const circle = `<circle cx="25" cy="25" r="20"/>`;
 const square = `<rect x="10" y="10" width="30" height="30"/>`;
 const triangle = `<polygon points="25,5 5,45 45,45"/>`;
 
-// Variable to add text to svg
+function renderShape(shape) {
+    if (shape === 'circle') {
+        return circle;
+    } else if (shape === 'square') {
+        return square;
+    } else if (shape === 'triangle') {
+        return triangle;
+    } else {
+        return "(err)";
+    }
+}
+
+// Variable to add text to cvg
 const cvgText = `<text x="50%" y="50%" fill="black" font-size="30" text-anchor="middle" dy=".3em">${data.text}</text>`;
+ function generateTextCvg(cvgText) {
+    return cvgText;
+ }
+// Variable to add color to text to cvg
+const cvgColor = `<rect width="100%" height="100%" fill=${data.textColor} />`;
 
-// Variable to add color to svg
-const cvgColor = `<rect width="100%" height="100%" fill=${data.color} />`;
+function generateTextColorCvg(cvgColor) {
+    if (data.textColor === 'red') {
+        return reduce;
+    } else if (data.textColor === 'blue') {
+        return blue;
+    } else if (data.textColor === 'green') {
+        return green;
+    } else if (data.textColor === 'yellow') {
+        return yellow;
+    } else if (data.textColor === 'purple') {
+        return purple;
+    } else if (data.textColor === 'orange') {
+        return orange;
+    } else {
+        return "(err)";
+    }
+}
 
-// Variable to add shape to svg
-const cvgShape = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${cvgColor}${cvgText}${data.shape}</svg>`;
+// Function to add shapeColor to cvg
+function generateShapeColorCvg(cvgColor) {
+    if (data.shapeColor === 'red') {
+        return red;
+    }
+    else if (data.shapeColor === 'blue') {
+        return blue;
+    }
+    else if (data.shapeColor === 'green') {
+        return green;
+    }
+    else if (data.shapeColor === 'yellow') {
+        return yellow;
+    }
+    else if (data.shapeColor === 'purple') {
+        return purple;
+    }
+    else if (data.shapeColor === 'orange') {
+        return orange;
+    }
+    else {
+        return "(err)";
+    }
+}
 
 
 
