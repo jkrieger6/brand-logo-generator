bff6b626546f3d4f1d790f46bc86187562d6f0eb
jkrieger6 <jkrieger11@gmail.com>
2023-04-22 09:00:37 -0700

    added shapes file to define shapes

---
+++ assets/shapes.js
@@ -0,0 +1,6 @@
+// Shapes variables
+const circle = `<circle cx="25" cy="25" r="20"/>`;
+const square = `<rect x="10" y="10" width="30" height="30"/>`;
+const triangle = `<polygon points="25,5 5,45 45,45"/>`;
+
+
