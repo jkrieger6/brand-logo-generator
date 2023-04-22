889ca134a09feca8ab18aca4e0a056ff015890e8
jkrieger6 <jkrieger11@gmail.com>
2023-04-22 08:24:10 -0700

    removed extra file to generate cvg and moved it to index.js

--- assets/generateCVG.js
+++
@@ -1,9 +0,0 @@
-// Function that generates the CVG file
-function generateCVG(answers) {
-    return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="300">
-        <text x="50" y="50" font-size="50" fill="${answers.textColor}">${answers.text}</text>
-        <${answers.shape} cx="200" cy="200" r="100" fill="${answers.shapeColor}"/>
-    </svg>
-    `;
-}
