0c79559df9835351318bc869742f2ad21eab6392
jkrieger6 <jkrieger11@gmail.com>
2023-04-24 11:55:56 -0700

    added possible class for shapes

--- assets/shapes.js
+++ assets/shapes.js
@@ -1,6 +1,17 @@
 const index = require("../index.js");
 
 // Shapes variables
+// class Shapes {
+//   constructor(shape, shapeColor) {
+//     this.shape = shape;
+//     this.shapeColor = shapeColor;
+//   };
+
+//   if (data.shape === "circle" || data.shape === "square" || data.shape === "trangle") {
+//     return Shapes;
+//   }
+// };
+
 const circle = `<circle cx="25" cy="25" r="20"/>`;
 const square = `<rect x="10" y="10" width="30" height="30"/>`;
 const triangle = `<polygon points="25,5 5,45 45,45"/>`;
