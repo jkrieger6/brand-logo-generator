b9403265765bc25312eb6e1d786eb2e1cc45eed9
jkrieger6 <jkrieger11@gmail.com>
2023-04-24 11:25:33 -0700

    reformatted with prettier

--- assets/shapes.js
+++ assets/shapes.js
@@ -1,4 +1,4 @@
-const index = require('../index.js');
+const index = require("../index.js");
 
 // Shapes variables
 const circle = `<circle cx="25" cy="25" r="20"/>`;
@@ -7,92 +7,87 @@ const triangle = `<polygon points="25,5 5,45 45,45"/>`;
 
 // Function to render shape on user input
 function renderShape(shape) {
-    if (shape === 'circle') {
-        return circle;
-    } else if (shape === 'square') {
-        return square;
-    } else if (shape === 'triangle') {
-        return triangle;
-    } else {
-        return "Please select a shape from the list.";
-    }
+  if (shape === "circle") {
+    return circle;
+  } else if (shape === "square") {
+    return square;
+  } else if (shape === "triangle") {
+    return triangle;
+  } else {
+    return "Please select a shape from the list.";
+  }
 }
 
 // Function to add text to svg
 const svgText = data.text;
- function renderTextSvg(text) {
-    return svgText;
- }
+function renderTextSvg(text) {
+  return svgText;
+}
 // Function to add color to text to svg
 const svgTextColor = data.textColor;
 
 function generateTextColorSvg(textColor) {
-    if (data.textColor === 'red') {
-        return chalk.red(data.text);
-    } else if (data.textColor === 'blue') {
-        return chalk.blue(data.text);
-    } else if (data.textColor === 'green') {
-        return chalk.green(data.text);
-    } else if (data.textColor === 'yellow') {
-        return chalk.yellow(data.text);
-    } else if (data.textColor === 'purple') {
-        return chalk.purple(data.text);
-    } else if (data.textColor === 'orange') {
-        return chalk.orange(data.text);
-    } else if (data.textColor === 'black') {
-        return chalk.black(data.text);
-    }
+  if (data.textColor === "red") {
+    return chalk.red(data.text);
+  } else if (data.textColor === "blue") {
+    return chalk.blue(data.text);
+  } else if (data.textColor === "green") {
+    return chalk.green(data.text);
+  } else if (data.textColor === "yellow") {
+    return chalk.yellow(data.text);
+  } else if (data.textColor === "purple") {
+    return chalk.purple(data.text);
+  } else if (data.textColor === "orange") {
+    return chalk.orange(data.text);
+  } else if (data.textColor === "black") {
+    return chalk.black(data.text);
+  }
 }
 
-// Or do i just make a function to check if the color is truthy and 
+// Or do i just make a function to check if the color is truthy and
 // then return the color and error if false?
 // Example:
 //function generateShapeColorSvg(shapeColor) {
 //   if (data.shapeColor === true) {
-    //     return chalk.color(data.shape);
-    // } else {
-    //     return "Please enter a valid color.";
+//     return chalk.color(data.shape);
+// } else {
+//     return "Please enter a valid color.";
 
 // Function to add shapeColor to svg
 function generateShapeColorSvg(shapeColor) {
-    if (data.shapeColor === 'red') {
-        return red;
-    }
-    else if (data.shapeColor === 'blue') {
-        return blue;
-    }
-    else if (data.shapeColor === 'green') {
-        return green;
-    }
-    else if (data.shapeColor === 'yellow') {
-        return yellow;
-    }
-    else if (data.shapeColor === 'purple') {
-        return purple;
-    }
-    else if (data.shapeColor === 'orange') {
-        return orange;
-    }
-    else {
-        return "(err)";
-    }
+  if (data.shapeColor === "red") {
+    return red;
+  } else if (data.shapeColor === "blue") {
+    return blue;
+  } else if (data.shapeColor === "green") {
+    return green;
+  } else if (data.shapeColor === "yellow") {
+    return yellow;
+  } else if (data.shapeColor === "purple") {
+    return purple;
+  } else if (data.shapeColor === "orange") {
+    return orange;
+  } else {
+    return "(err)";
+  }
 }
 
 // Function that returns color choice to svg
 function renderColor(textColor, shapeColor) {
-    return ` 
+  return ` 
     ${generateShapeColorSvg(shapeColor)} 
     ${generateTextColorSvg(textColor)}`;
 }
 
 // Function to generate svg file
 function generateSVG(data) {
-    return `
+  return `
     <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        "${renderShape(data.shape)}${(renderColor(textColor, shapeColor))}"
-        <text x="50%" y="50%" fill="${data.textColor}" font-size="30" text-anchor="middle" dy=".3em">${data.text}</text>
+        "${renderShape(data.shape)}${renderColor(textColor, shapeColor)}"
+        <text x="50%" y="50%" fill="${
+          data.textColor
+        }" font-size="30" text-anchor="middle" dy=".3em">${data.text}</text>
         </svg>`;
 }
 
 module.exports = generateSVG;
-
