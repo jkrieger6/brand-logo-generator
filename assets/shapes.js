12da27738aaf3b09445fd745fde22fe5f1aa3cf0
jkrieger6 <jkrieger11@gmail.com>
2023-04-25 10:02:11 -0700

    added test to make functions but commented test out

--- assets/shapes.js
+++ assets/shapes.js
@@ -1,6 +1,6 @@
 const index = require("../index.js");
 
-
+// Test to check if the renderShape function returns  when chosen as shape
 //const shape = new Triangle();
 // shape.setColor("blue");
 // expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
