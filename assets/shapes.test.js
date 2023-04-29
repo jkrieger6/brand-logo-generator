b02c1b28660df2fbca630d0f56ccb9302da640a5
jkrieger6 <jkrieger11@gmail.com>
2023-04-29 09:38:55 -0700

    trying more tests

--- assets/shapes.test.js
+++ assets/shapes.test.js
@@ -1,5 +1,5 @@
 const Shapes = require('./shapes.js');
-const renderShape = require('./shapes.js')
+const generateSVG = require('./shapes.js')
 const Circle = require('./shapes.js');
 const Square = require('./shapes.js');
 const Triangle = require('./shapes.js');
@@ -17,12 +17,19 @@ describe('Shapes', () => {
     it('should exist', () => {
         expect(Shapes).toBeDefined();
     })
+});
 // Test to check if the Shapes function returns a green circle when chosen as shape
-describe('Circle', () => {
+describe('generateSVG', () => {
     it('should return a green circle when chosen', () => {
         const shape = new Circle();
         shape.setColor("green");
-        expect(shape.render()).toEqual('<circle cx="150" cy="100" r="82" fill="green"/>');
+        expect(shape.render()).toEqual(`
+        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+           <g> ${renderShape(data.shape, data.shapeColor)}</g>
+            <text x="150" y="125" fill="${
+              data.textColor
+            }" font-size="60" text-anchor="middle">${data.text}</text>
+        </svg>`);
     });
 });
 // Test to check if the renderShape function returns a red square when chosen as shape
@@ -45,7 +52,7 @@ describe('Triangle', () => {
 });
 
 
-});
+
 
 
 
