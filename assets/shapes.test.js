34b519cb025a5fd8cbd9331c0db469d89efe0e7a
jkrieger6 <jkrieger11@gmail.com>
2023-04-29 21:50:16 -0700

    all tests passing now

--- assets/shapes.test.js
+++ assets/shapes.test.js
@@ -1,9 +1,10 @@
 const Shapes = require('./shapes.js');
-const generateSVG = require('./shapes.js')
+const renderShape = require('./shapes.js');
 const Circle = require('./shapes.js');
 const Square = require('./shapes.js');
 const Triangle = require('./shapes.js');
 const textObj = require('../index.js'); 
+
 
 // Test to see if error is thrown when number of characters is greater than 3
 describe('textObj', () => {
@@ -18,29 +19,26 @@ describe('Shapes', () => {
         expect(Shapes).toBeDefined();
     })
 });
-// Test to check if the Shapes function returns a green circle when chosen as shape
-describe('generateSVG', () => {
-    it('should conatin a green circle in the SVG', () => {
-        const shape = new Circle();
-        shape.setColor("green");
-        expect(shape.render()).toContain(`<circle cx="150" cy="100" r="82" fill="green"/>`)
+// Test to check if the renderShape function returns a green circle when chosen as shape
+describe('Circle', () => {
+    it('should contain a green circle in the SVG', () => {
+        const greenCircle = Shapes.renderShape('circle', 'green');
+        expect(greenCircle).toEqual(`<circle cx="150" cy="100" r="82" fill="green"/>`)
     });
 });
 // Test to check if the renderShape function returns a red square when chosen as shape
 describe('Square', () => {
     it('should return a red square when chosen', () => {
-        const shape = new Square();
-        shape.setColor("red");
-        expect(shape.render()).toEqual('<rect x="56" y="18" width="188" height="164" fill="red"/>');
+       const redSquare = Shapes.renderShape('square', 'red');
+        expect(redSquare).toEqual('<rect x="56" y="18" width="188" height="164" fill="red"/>');
     });
 });
 
 // Test to check if the renderShape function returns a blue triangle when chosen as shape
 describe('Triangle', () => {
     it('should return a blue triangle when chose as shape', () => {
-        const shape = new Triangle();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+        const blueTriangle = Shapes.renderShape('triangle', 'blue');
+        expect(blueTriangle).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue"/>');
 
     });
 });
